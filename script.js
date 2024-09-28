0ab57f2c58d4a1db9e16fe2cc1b5d31e2ab651c2
Gabriel Leonardo <168693583+gabirusz@users.noreply.github.com>
2024-09-28 14:42:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || (fano.value) > ano ) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement ('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+        if ( idade >=0 && idade < 10 ){
+            img.setAttribute('src' , 'foto.criança mas.jpeg')
+            genero = 'Criança'
+            // Criança 
+        } else if ( idade < 21 ){
+            img.setAttribute('src', 'foto.jovem.jpeg')
+            genero = 'Jovem'
+            // Jovem 
+        } else if ( idade < 50 ){
+            img.setAttribute('src','foto.homem.jpeg')
+            genero = 'Adulto'
+            // Adulto  
+        } else {
+            img.setAttribute('src', 'foto.senhor.jpeg')
+            genero = 'Idoso'
+            // Idoso
+        }
+        } else if (fsex[1].checked) {
+        if ( idade >=0 && idade < 10 ){
+            img.setAttribute('src', 'foto.criança fem.jpeg')
+            genero = 'Criança'
+                    // Criança 
+        } else if ( idade < 21 ){
+            img.setAttribute('src', 'foto.jovem fem.jpeg')
+            genero = 'Jovem'
+                    // Jovem 
+        } else if ( idade < 50 ){
+            img.setAttribute('src', 'foto.muler.jpeg')
+            genero = 'Adulto'
+                    // Adulto  
+        } else {
+            img.setAttribute('src', 'foto.senhora.jpeg')
+            genero = 'Idoso'
+                    // Idoso
+        }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero}(a) com ${idade} anos. `
+        res.appendChild(img)
+    }
+}
